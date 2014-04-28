bb604c7c06d082de47aa87716a59c935a205feea
Chris3037 <Chris3037@gmail.com>
2014-04-27 23:48:21 -0600

    V 3.0
    Everything works

--- Week3Code Challenge/Week3Code Challenge/Program.cs
+++ Week3Code Challenge/Week3Code Challenge/Program.cs
@@ -11,8 +11,10 @@ namespace Week3Code_Challenge
         static void Main(string[] args)
         {
             FindManyPrimes(10001);
-            //EvenFibonacciSequencer(2000000);
-            //LongestCollatzSequencer();
+            Console.WriteLine();
+            EvenFibonacciSequencer(2000000);
+            Console.WriteLine();
+            LongestCollatzSequencer();
             End();
         }
 
@@ -20,9 +22,9 @@ namespace Week3Code_Challenge
         static void FindManyPrimes(int thisMany)
         {
             int primeNums = 0;
-            int number = 1;
+            int number = 2;
 
-            while(primeNums < thisMany)
+            while (primeNums < thisMany)
             {
                 if (IsPrime(number))
                 {
@@ -30,40 +32,90 @@ namespace Week3Code_Challenge
                 }
                 number++;
             }
-            Console.WriteLine(number-1);
+            number--;
+            Console.WriteLine("The " + thisMany + "st prime number is " + number);
         }
 
         //IsPrime
         public static bool IsPrime(int number)
         {
-            if (number == 2 || number == 3 || number == 5 || number == 7)
+            for (int i = 2; i < number; i++)
             {
-                return true;
+                if (number % i == 0)
+                {
+                    return false;
+                }
             }
-            else if (number % 1 == 0 && number % number == 0 && number % 2 != 0 && number % 3 != 0 && number % 4 != 0 && number % 5 != 0 && number % 6 != 0 && number % 7 != 0 && number % 8 != 0 && number % 9 != 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return true;
         }
 
         //EvenFibonacciSequencer Function
         static void EvenFibonacciSequencer(int maxValue)
         {
-
+            int number = 2;
+            int fibNum = 0;
+            int fibOne = 1;
+            int fibTwo = 1;
+            int fibTemp = 0;
+            while (number < maxValue)
+            {
+                if (fibOne % 2 == 0)
+                {
+                    fibNum += fibOne;
+                }
+                fibTemp = fibOne;
+                number += fibOne;
+                fibOne += fibTwo;
+                fibTwo = fibTemp;
+            }
+            Console.WriteLine("The " + number + "th fibonacci number (minus odds) is " + fibNum);
         }
 
         //LongestCollatzSequencer Function
         static void LongestCollatzSequencer()
         {
-            int counter = 0;
-            for (int i = 0; i == 1 ; i++)
+            int onNum = 0;
+            int highestCounter = 0;
+            int newCounter = 0;
+            int highestNum = 0;
+            int highestSegment = 0;
+
+            for (int i = 1; i <= 1000000; i++)
             {
+                onNum++;
+                long number = i;
+                int counter = 0;
+                int segments = 1;
+
+                while (number != 1)
+                {
+                    if (number % 2 == 0)
+                    {
+                        number = number / 2;
+                        segments++;
+                    }
+                    else
+                    {
+                        number = number * 3;
+                        number = number + 1;
+                        segments++;
+                    }
+                    counter++;
+                }
+
+                newCounter = counter;
+                if (highestCounter < newCounter)
+                {
+                    highestCounter = counter;
+                    highestNum = onNum;
+                }
+                if (highestSegment < segments)
+                {
+                    highestSegment = segments;
+                }
             }
-                Console.WriteLine(counter);
+            Console.WriteLine("The number that produces the longest chain is: " + highestNum);
+            Console.WriteLine("That chain is " + highestSegment + " segments long.");
         }
 
         //End Function
